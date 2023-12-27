5c010d67e8c89569251903ba4f00263bd99d1617
Shaikh Adnan <117739824+Sha1kh4@users.noreply.github.com>
2023-12-28 01:11:33 +0530

    Added Error Handling and clearing previous output

--- extension/popup.js
+++ extension/popup.js
@@ -1,26 +1,43 @@
 const btn = document.getElementById("find");
+const outputContainer = document.getElementById("output");
+
 btn.addEventListener("click", function() {
     btn.disabled = true;
     btn.innerHTML = "Finding length...";
+    outputContainer.innerHTML = "";  // Clear previous results
+
     chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-        var url = tabs[0].url;
-        var xhr = new XMLHttpRequest();
+        const url = tabs[0].url;
+        const xhr = new XMLHttpRequest();
         xhr.open("GET", "https://yt-2znm.onrender.com/summary?url=" + url, true);
         xhr.responseType = 'json';
+
         xhr.onload = function() {
-            var jsonResponse = xhr.response;
-            var htmlString = "";
-            for (var key in jsonResponse) {
-                if (jsonResponse.hasOwnProperty(key)) {
-                    htmlString += key + ": " + jsonResponse[key] + "<br>";
+            if (xhr.status === 200) {
+                const jsonResponse = xhr.response;
+                let htmlString = "";
+
+                for (const key in jsonResponse) {
+                    if (jsonResponse.hasOwnProperty(key)) {
+                        htmlString += key + ": " + jsonResponse[key] + "<br>";
+                    }
                 }
+
+                outputContainer.innerHTML = htmlString;
+            } else {
+                outputContainer.innerHTML = "Error: Unable to fetch playlist information.";
             }
-            const p = document.getElementById("output");
-            p.innerHTML = htmlString;
 
             btn.disabled = false;
             btn.innerHTML = "Find Length";
         }
+
+        xhr.onerror = function() {
+            outputContainer.innerHTML = "Error: Unable to connect to the server.";
+            btn.disabled = false;
+            btn.innerHTML = "Find Length";
+        }
+
         xhr.send();
     });
 });
