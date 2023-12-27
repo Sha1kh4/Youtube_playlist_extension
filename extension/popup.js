2586c1dc6eb47cdbe0d897ae449d686892183f7d
Shaikh Adnan <117739824+Sha1kh4@users.noreply.github.com>
2023-12-28 01:03:48 +0530

    hosted api and linked

--- extension/popup.js
+++ extension/popup.js
@@ -5,7 +5,7 @@ btn.addEventListener("click", function() {
     chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
         var url = tabs[0].url;
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
+        xhr.open("GET", "https://yt-2znm.onrender.com/summary?url=" + url, true);
         xhr.responseType = 'json';
         xhr.onload = function() {
             var jsonResponse = xhr.response;
